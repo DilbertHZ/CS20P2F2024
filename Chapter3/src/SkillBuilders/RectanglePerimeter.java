c5fbe0fce44635503a3d38c5ba0eaf422667e173
48004001 <48004001@L815WD220025.edu.cbe.ab.ca>
2024-09-11 11:27:38 -0600

    11:30

---
+++ Chapter3/src/SkillBuilders/RectanglePerimeter.java
@@ -0,0 +1,24 @@
+package SkillBuilders;
+
+import java.util.*;
+
+public class RectanglePerimeter 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner userInput = new Scanner(System.in);
+		
+		System.out.print("Enter length of rectangle: ");
+		int length = userInput.nextInt();
+		
+		System.out.print("Enter width of rectangle: ");
+		int width = userInput.nextInt();
+		
+		int area = length * width;
+
+		System.out.print("The are of the rectangle is: " + area);
+	}
+
+}
