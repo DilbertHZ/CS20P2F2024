0008a809be999571c61e6d05c41fae1000dcdf97
48004001 <48004001@L815WD220025.edu.cbe.ab.ca>
2024-11-25 11:58:43 -0700

    11:58

--- Chapter7/src/Mastery/MySavings.java
+++ Chapter7/src/Mastery/MySavings.java
@@ -1,13 +1,31 @@
 package Mastery;
 
 public class MySavings {
+	
+	public static double totalValue;
 
-	public double PiggyBank(MySavings input) {
+	public static void PiggyBank(int x) {
+		switch (x) {
+		case 0: 
+			break;
+		case 1: 
+			System.out.println("You have " + totalValue + " in your piggybank.");
+		case 2: 
+			totalValue += 0.01;
+		case 3:
+			totalValue += 0.05;
+		case 4:
+			totalValue += 0.1;
+		case 5:
+			totalValue += 0.25;
+		case 6:
+			totalValue = 0;
+		}
+	}
+
+	public static void x(int nextInt) {
+		// TODO Auto-generated method stub
 		
-		
-		
-		double total = 0;
-		return total;
 	}
 
 
--- Chapter7/src/Mastery/TestMySavings.java
+++ Chapter7/src/Mastery/TestMySavings.java
@@ -3,25 +3,26 @@ package Mastery;
 import java.util.Scanner;
 
 public class TestMySavings {
-
+	
 	public static void main(String[] args) { 
-		
-		//Prompt user for input
-		System.out.println("1. Show total in bank.");
-		System.out.println("2. Add a penny.");
-		System.out.println("3. Add a nickel.");
-		System.out.println("4. Add a dime.");
-		System.out.println("5. Add a quarter.");
-		System.out.println("6. Take money out of bank.");
-		System.out.println("Enter 0 to quit.");
-		System.out.println("Enter your choice: ");
-		
-		//Preparing and recording user input
-		Scanner userInput = new Scanner(System.in);
-
-		MySavings input = new MySavings();
-		input.PiggyBank(input);
-		
-	}
+	
+		while (userInput.nextInt() != 0)
+			//Prompt user for input
+			System.out.println("1. Show total in bank.");
+			System.out.println("2. Add a penny.");
+			System.out.println("3. Add a nickel.");
+			System.out.println("4. Add a dime.");
+			System.out.println("5. Add a quarter.");
+			System.out.println("6. Take money out of bank.");
+			System.out.println("Enter 0 to quit.");
+			System.out.println("Enter your choice: ");
+			
+			//Preparing and recording user input
+			Scanner userInput = new Scanner(System.in);
+			
+			MySavings PiggyBank = new MySavings();
+			MySavings.PiggyBank(userInput.nextInt());
+		}
+	
 
 }
